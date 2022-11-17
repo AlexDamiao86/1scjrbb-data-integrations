c065d13f0d0e7af908d60908cedbd7f670fd6e3b
AlexDamiao86 <alex_maiabr@hotmail.com>
2022-11-16 22:47:16 -0300

    Envia e-mail através de conta GMAIL para endereço configurado em application.yml

---
+++ email-consumer/src/main/java/br/com/fiap/emailconsumer/service/EmailService.java
@@ -0,0 +1,9 @@
+package br.com.fiap.emailconsumer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface EmailService {
+    void sendSimpleMessage(String to, String subject, String text);
+    void sendSimpleMessageUsingTemplate(String to, String subject, String ...templateModel);
+}
---
+++ email-consumer/src/main/java/br/com/fiap/emailconsumer/service/EmailServiceImpl.java
@@ -0,0 +1,41 @@
+package br.com.fiap.emailconsumer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("EmailService")
+public class EmailServiceImpl implements EmailService {
+
+    private static final String NOREPLY_ADDRESS = "noreply@fiap.com.br";
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Autowired
+    private SimpleMailMessage emailTemplate;
+
+    @Override
+    public void sendSimpleMessage(String to, String subject, String text) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(NOREPLY_ADDRESS);
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+
+            emailSender.send(message);
+        } catch (MailException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public void sendSimpleMessageUsingTemplate(String to, String subject, String... templateModel) {
+        String text = String.format(emailTemplate.getText(), templateModel);
+        sendSimpleMessage(to, subject, text);
+    }
+}
+
