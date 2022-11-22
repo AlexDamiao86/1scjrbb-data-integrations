2ce6c0795e54aafb5eb2667ed3d8c5875757258b
AlexDamiao86 <alex_maiabr@hotmail.com>
2022-11-22 15:53:02 -0300

    Altera valores constantes Scheduler.java

--- drone-producer/src/main/java/br/com/fiap/droneproducer/service/Scheduler.java
+++ drone-producer/src/main/java/br/com/fiap/droneproducer/service/Scheduler.java
@@ -16,19 +16,19 @@ public class Scheduler {
     private Double temp = 12.0;
     private static final Double MAX_TEMP = 40.0;
 
-    private Double umidade = 100.0;
+    private Double umidade = 90.0;
     private static final Double MIN_UMIDADE = 10.0;
 
     private boolean rastreamento = false;
 
     @Scheduled(fixedRate = 10000)
     public void sendMessage() {
-        if (temp <= MAX_TEMP) temp++;
+        if (temp < MAX_TEMP) temp++;
         Drone drone1 = new Drone(9999999L, -80.54, 120.87, temp, 50.0, true);
         System.out.println("Auto drone " + drone1.getId() + " enviou mensagem.. ");
         droneProducer.send(drone1);
 
-        if (umidade >= MIN_UMIDADE) umidade--;
+        if (umidade > MIN_UMIDADE) umidade--;
         Drone drone2 = new Drone(8888888L, -72.23, 170.87, 34.0, umidade, true);
         System.out.println("Auto drone " + drone2.getId() + " enviou mensagem.. ");
         droneProducer.send(drone2);
